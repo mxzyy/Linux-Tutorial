c2ec75f1f710e76d2399d43a5b8ff7ade2ca5a39
Hilmy Pratama <94878524+mxzyy@users.noreply.github.com>
2024-08-08 10:52:31 +0700

    Create k8s.sh

---
+++ k8s.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+apt install ca-certificates curl apt-transport-https curl gnupg gnupg2 software-properties-common curl -y
+
+# SWAPOFF
+swapoff -a
+
+# MODPROBE MODULES
+tee /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+# ADD KERNEL PARAMS FOR K8S
+tee /etc/sysctl.d/kubernetes.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sysctl --system
+
+
+# Docker Repo
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# K8s Repo
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+chmod 644 /etc/apt/sources.list.d/kubernetes.list
+
+# pt updet
+apt update
